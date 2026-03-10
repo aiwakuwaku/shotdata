ffab5ed75ec28966569ae53317e1da9db6b655cc
aiwakuwaku <parplay1872@gmail.com>
2026-03-10 11:49:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-const CACHE_NAME = 'golf-pwa-cache-v1';
+const CACHE_NAME = 'golf-log-v1';
+// キャッシュするファイルのリスト
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-round.svg',
+  'https://cdn.tailwindcss.com' // デザイン用のプログラムも保存する
+];
+
+// インストール時にファイルをスマホに保存
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
+
+// オフライン時はスマホに保存したファイルを返す
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
